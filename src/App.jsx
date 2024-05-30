77966e68f96c1d294beb420861d3595774729cfd
Leonald <leufrasiojunior@gmail.com>
2024-05-30 10:21:19 -0300

    Font Changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
 
 function App() {
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log("login");
+  };
+
   return (
     <Container
       fluid
       className="d-flex vh-100 justify-content-center align-items-center bg-light-custom"
     >
-      <Row className="w-100">
+      <Row className="w-100 font-inter anti-aliased">
         <Col xs={12} sm={8} md={6} lg={4} className="mx-auto">
           <Container className="bg-white rounded-4 shadow-sm p-5 border custom-ring">
             <h2 className="text-center">TurboTracker</h2>
@@ -27,7 +32,11 @@ function App() {
               <Form.Group controlId="formBasicCheckbox" className="text-start">
                 <Form.Check type="checkbox" label="Remember me" />
               </Form.Group>
-              <Button type="submit" className="w-100 custom-bg-button">
+              <Button
+                type="submit"
+                className="w-100 custom-bg-button"
+                onClick={handleLogin}
+              >
                 Sign in
               </Button>
             </Form>
